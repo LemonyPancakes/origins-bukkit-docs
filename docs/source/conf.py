66378b6f7c742af9da423cb4bf4078a58d9c8897
LemonyPancakes <JiboyJUNE@gmail.com>
2022-08-29 12:41:13 +0800

    favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 html_favicon = 'Origins-Bukkit-256x.ico'
 
 # -- Options for EPUB output
